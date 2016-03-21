f92b648e55fd36ee015b7c60468b6ea1fe23f65c
Michael Toth <mt.toth@gmail.com>
2016-03-20 22:39:03 -0400

    Updating rss feeds

--- publishconf.py
+++ publishconf.py
@@ -15,6 +15,9 @@ FAVICON = SITEURL + '/images/favicon.ico'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM =	'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
